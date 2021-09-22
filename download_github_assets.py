7e1599216c956127846d30222182bf72fe331888
Bruce Hill <bhill@slac.stanford.edu>
2021-09-22 15:32:27 -0700

    Added command line options via argparse instead of hard coding repo and tags.
    Also allows selecting assets via a list of regex expressions.

--- download_github_assets.py
+++ download_github_assets.py
@@ -1,26 +1,37 @@
 import os
 import urllib.request
 import os.path
+import argparse
 import json
+import re
+import sys
+import textwrap
 
-def download_asset(url, filename, token=None):
+def download_asset(url, filename, token=None, verbose=False):
     headers = {}
-    
+
     headers["Accept"] = 'application/octet-stream'
     if token:
         headers["Authorization"] = "token {}".format(token)
 
+    if verbose:
+        print( 'download url={}, headers={}'.format(url,headers) )
     request = urllib.request.Request(url, headers=headers)
 
-    with urllib.request.urlopen(request) as response:
-        with open(filename, "wb") as fp:
-            fp.write(response.read())
+    try:
+        with urllib.request.urlopen(request) as response:
+            with open(filename, "wb") as fp:
+                fp.write(response.read())
+                print( "{}: OK".format(filename) )
+    except urllib.error.HTTPError as e:
+        print( 'HTTPError: {}'.format(e) )
+        print( "{}: Error {}".format(filename,e) )
 
-def get_assets(owner, repo, tag=None, token=None):
+def get_assets(repo, re_assets=None, tag=None, token=None):
     if tag:
-        url = "https://api.github.com/repos/{}/{}/releases/tags/{}".format(owner, repo, tag)
+        url = "https://api.github.com/repos/{}/releases/tags/{}".format(repo, tag)
     else:
-        url = "https://api.github.com/repos/{}/{}/releases".format(owner, repo)
+        url = "https://api.github.com/repos/{}/releases".format(repo)
 
     headers = {}
     if token:
@@ -32,26 +43,58 @@ def get_assets(owner, repo, tag=None, token=None):
             tags = [content]
         else:
             tags = content
-        
+
         for tag_data in tags:
             for asset in tag_data['assets']:
-                yield ("{}?access_token={}".format(asset['url'], token) if token else asset['url']), asset['name']
+                asset_url="{}?access_token={}".format(asset['url'], token) if token else asset['url']
+                if re_assets is None:
+                    yield (asset_url), asset['name']
+                for re_asset in re_assets:
+                    try:
+                        if re.match(re_asset,asset['name']):
+                            yield (asset_url), asset['name']
+                    except re.error as e:
+                        print( 'Invalid regular expression: {}'.format(re_asset) )
+                        print( e )
+                        return
 
-if __name__ == '__main__':
-    owner='REPO_OWNER'
-    repo='REPO_NAME'
-    token = 'PRIVATE_ACCESS_TOKEN OPETIONAL'
-    tags = [
-        'RELEASE_TAGS_CONTAININGP_ASSETS'
-    ]
+def process_options(argv):
+    if argv is None:
+        argv = sys.argv[1:]
+    description =	'download_github_assets.py supports downloading selected assets from github public and private repos.\n'
+    epilog_fmt  =	'\nExamples:\n' \
+                    'download_github_assets.py -a "*.mcs" -r slaclab/wave8\n'
+    epilog = textwrap.dedent( epilog_fmt )
+    parser = argparse.ArgumentParser( description=description, formatter_class=argparse.RawDescriptionHelpFormatter, epilog=epilog )
+    parser.add_argument( '-a', '--assets',   dest='assets', action='append', \
+                        help='Regex for assets to download. Ex: *.gz', default=[] )
+    parser.add_argument( '-t', '--tags',     action="append", help='release tag to download.' )
+    parser.add_argument( '-r', '--repo',	 action='store',  help='Github repo.  Ex. slaclab/wave8' )
+    parser.add_argument( '-v', '--verbose',  action="store_true", help='show more verbose output.' )
+
+    options = parser.parse_args( )
+    return options 
+
+def main(argv=None):
+    options = process_options(argv)
+
+    token_path = os.getenv('GH_TOKEN')
+    token = None
+    if token_path and os.path.isfile(token_path):
+        with open(token_path) as f:
+            token = f.read().strip()
 
     directory = '.'
 
     os.makedirs(directory, exist_ok=True)
 
-    for tag in tags:
-        assets = get_assets(owner=owner, repo=repo, tag=tag, token=token)
+    for tag in options.tags:
+        assets = get_assets(repo=options.repo, re_assets=options.assets, tag=tag, token=token)
         for url, filename in assets:
             path = os.path.join(directory, filename)
-            download_asset(url, filename)
+            download_asset(url, filename, token)
+    return 0
 
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
